b6ad08ad1ca558b548e35cccbdb978993f96a994
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-10-04 13:07:17 +0300

    New_task_1

--- 02/tasks.js
+++ 02/tasks.js
@@ -3,9 +3,8 @@
  * Доп. задание: предложите несколько вариантов решения.
  */
 function timer(logger = console.log) {
-  for (let i = 0; i < 10; i++) {
-    logger(i);
-    setTimeout(() => {}, 1000);
+  for (var i = 1; i <10; i++) {
+    (function(index) {setTimeout( () => { logger(index); }, 100); })(i);
   }
 }
 
