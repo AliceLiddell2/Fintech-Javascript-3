3ab2b707cded4d2e7e8e0e1f2854308e00da3c74
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-10-06 03:04:00 +0300

    Task_6_minor_update

--- 02/tasks.js
+++ 02/tasks.js
@@ -87,9 +87,12 @@ function getIntersection(first, second) {
   first.forEach(element => cache[element] > 0 ? cache[element] += 1 : cache[element] = 1);
 
   second.forEach(element => {
-    if (cache[element] > 0) {
-      result.push(element);
-      cache[element] -= 1;
+    if (cache[element] !== undefined)
+    {
+      if (cache[element] > 0) {
+        result.push(element);
+        cache[element] -= 1;
+      }
     }
   });
   return result.sort((first, second) => first - second);
