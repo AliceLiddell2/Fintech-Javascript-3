2e3e9e68e510f53e725c3e3494b142a08f594749
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-10-12 16:51:28 +0300

    Task_2_corr

--- 03/2/task.js
+++ 03/2/task.js
@@ -7,6 +7,7 @@
 * @param {Number} timeoutInMilliseconds время для timeout в миллисекундах
 * @return {Promise} промис с нужным поведением
 */
+/*
 function promiseRace(promises) {
   return new Promise((resolve, reject) => {
     promises.forEach(promise => promise.then(resolve).catch(reject));
@@ -26,6 +27,21 @@ function rejectOnTimeout(promise, timeoutInMilliseconds) {
     promise,
     timeout
   ]);
+}*/
+function rejectOnTimeout(promise, timeoutInMilliseconds) {
+    let timeout;
+
+    timeout = new Promise((resolve, reject) => {
+        let id = setTimeout(() => {
+            clearTimeout(id);
+            reject('timeout_error');
+        }, timeoutInMilliseconds);
+    });
+    let promises = [promise, timeout];
+
+    return new Promise((resolve, reject) => {
+        promises.forEach(promise => promise.then(resolve).catch(reject));
+    });
 }
 
 module.exports = rejectOnTimeout;
