a855b6eea4ab68d508623a08ef613de2b4bc9e29
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-10-14 01:10:31 +0300

    typo_fix

--- 03/5/task.js
+++ 03/5/task.js
@@ -13,7 +13,7 @@ function toDecimal(s) {
     M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1
   };
 
-  s.replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, (d) => n += X[d]);
+  s.replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, d => n += X[d]);
 
   for (let i = 0; i < n; i++) {
     res[i] = i;
