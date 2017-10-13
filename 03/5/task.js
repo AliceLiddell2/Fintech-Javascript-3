678db0b4a32ba3d46cc4886b6f436ec4f4e006ce
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-10-14 01:08:34 +0300

    Task_5_typo_fix

--- 03/5/task.js
+++ 03/5/task.js
@@ -10,8 +10,7 @@ function toDecimal(s) {
   let n = 0;
   let res = [];
   let X = {
-    M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90,
-    L: 50,  XL: 40,  X: 10,  IX: 9,   V: 5,   IV: 4,  I: 1
+    M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1
   };
 
   s.replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, (d) => n += X[d]);
@@ -23,7 +22,7 @@ function toDecimal(s) {
 };
 
 let handler = {
-  get: (target, prop) => { return prop in target ? target[prop] : toDecimal(prop)}
+  get: (target, prop) => { return prop in target ? target[prop]; : toDecimal(prop);}
 }
 
 let p = new Proxy(/0./, handler);
