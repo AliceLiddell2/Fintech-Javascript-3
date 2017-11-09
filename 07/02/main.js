0aef9c2db9e05fcc42b0560f46087f6d70db0abd
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-11-09 23:04:37 +0300

    corr

--- 07/02/main.js
+++ 07/02/main.js
@@ -1,11 +1,11 @@
-let timeDistance = 150;
+const timeDistance = 150;
 const parentElement = document.getElementById('list');
 
 window.onload = function doubleClick(element, doubleClickHandler, timeDistance) {  
   element = document.getElementById('testButton');
   function getTimeStamp() {
-     let now = new Date();
-     return ((now.getMonth() + 1) + '-' + (now.getDate()) + '-' + now.getFullYear() + " " + now.getHours() + ':' + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds())));
+    let now = new Date();
+    return ((now.getMonth() + 1) + '-' + (now.getDate()) + '-' + now.getFullYear() + " " + now.getHours() + ':' + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds())));
  }
   
   element.onclick = () => {
@@ -14,9 +14,9 @@ window.onload = function doubleClick(element, doubleClickHandler, timeDistance) 
 
   element.ondblclick = () => {
     let newLi = document.createElement('li');
-
+    
     clearTimeout(timer);
     newLi.innerHTML = '2xClick' + ' - ' + getTimeStamp();
     list.appendChild(newLi);
-  }
-}
+  };
+};
