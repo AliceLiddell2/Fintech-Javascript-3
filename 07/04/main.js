4cedc054e971b5b619f060b9ed8159910c9022b4
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-11-11 03:14:23 +0300

    upd

---
+++ 07/04/main.js
@@ -0,0 +1,71 @@
+let k = 0;
+function getJson(k) {
+  const list = document.getElementById('list');
+  let xmlhttp = new XMLHttpRequest();
+  let url = xmlhttp.open("GET", "https://api.github.com/orgs/facebook/repos?page=1", true);
+
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let myObj = JSON.parse(this.responseText);
+
+      for (let i=k; i< k+1; i++) {
+        let newLi = document.createElement('li');
+
+        newLi.innerHTML = 'Name is ' + myObj[i].name + ':';
+        list.appendChild(newLi);
+        newLi = document.createElement('li');
+        newLi.innerHTML = 'descr: ' +myObj[i].description;
+        list.appendChild(newLi);
+        newLi = document.createElement('li');
+        newLi.innerHTML = 'url: ' + myObj[i].url;
+        list.appendChild(newLi);
+        newLi = document.createElement('li');
+        newLi.innerHTML = 'created: ' + myObj[i].created_at;
+        list.appendChild(newLi);
+        newLi = document.createElement('li');
+        newLi.innerHTML = 'commited: ' + myObj[i].pushed_at;
+        list.appendChild(newLi);
+        newLi = document.createElement('li');
+        newLi.innerHTML = '<br>';
+        list.appendChild(newLi);
+      }
+    }
+  };
+  xmlhttp.send();
+}
+
+let numElementsToAdd = 10;
+let offsetForNewContent = 20;
+
+function checkInfiniteScroll(parentSelector, childSelector) {
+    let lastDiv = document.querySelector(parentSelector + childSelector);
+    let lastDivOffset = lastDiv.offsetTop + lastDiv.clientHeight;
+    let pageOffset = window.pageYOffset + window.innerHeight;
+
+    if(pageOffset > lastDivOffset - offsetForNewContent ) {
+      for(var i = 0; i < numElementsToAdd; i++) {
+        let newDiv = document.createElement("div");
+
+        getJson(k);
+        k = k+1;
+        document.querySelector(parentSelector).appendChild(newDiv);
+      }
+      checkInfiniteScroll(parentSelector, childSelector);
+    }
+};
+
+let lastScrollTime = Date.now();
+let checkInterval = 50;
+
+function update() {
+  requestAnimationFrame(update);
+
+  let currScrollTime = Date.now();
+
+  if(lastScrollTime + checkInterval < currScrollTime) {
+    checkInfiniteScroll("#scroll-content", "> div:last-child");
+    lastScrollTime = currScrollTime;
+  }
+};
+
+update();
