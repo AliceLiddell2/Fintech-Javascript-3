6c6266d307523ea2b8800e9acb7eb8d56b48869e
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-11-15 17:26:35 +0300

    small upd

--- 07/02/main.js
+++ 07/02/main.js
@@ -2,37 +2,42 @@
 
 const list = document.getElementById('list');
 let clicks = 0;
-let a;
-let b;
+let time_sg_click;
+let time_db_click;
+let time;
 let pause = 0;
 
-window.onload = function doubleClick(element, doubleClickHandler, timeDistance = 250) {
+window.addEventListener('DOMContentLoaded', function doubleClick(element, doubleClickHandler, timeDistance = 250) {
   element = document.getElementById('test-button');
   function getTimeStamp() {
-    let now = new Date();
+      let now = new Date();
 
-    return ((now.getMonth() + 1) + '-' + (now.getDate()) + '-' + now.getFullYear() + ' ' + now.getHours() + ':' + ((now.getMinutes() < 10) ? ('0' + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ('0' + now.getSeconds()) : (now.getSeconds())));
+      return ((now.getMonth() + 1) + '-' + (now.getDate()) + '-' + now.getFullYear() + ' ' + now.getHours() + ':' + ((now.getMinutes() < 10) ? ('0' + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ('0' + now.getSeconds()) : (now.getSeconds())));
+    }
+  function doubleClickHandler(timeDate) {
+    let newLi = document.createElement('li');
+
+    newLi.innerHTML = '2xClick - ' + timeDate;
+    list.appendChild(newLi);
   }
 
   element.onclick = () => {
     clicks++;
     if ((clicks === 1) && (pause === 0)) {
-      a = new Date().getTime();
+      time_sg_click = new Date().getTime();
+      time = getTimeStamp();
     }
     if (clicks === 2) {
-      b = new Date().getTime();
-      if (b - a < timeDistance) {
-        let newLi = document.createElement('li');
-
-        newLi.innerHTML = '2xClick - ' + getTimeStamp();
-        list.appendChild(newLi);
+      time_db_click = new Date().getTime();
+      if (time_db_click - time_sg_click < timeDistance) {
+        doubleClickHandler(time);
         clicks = 0;
         pause = 0;
       } else {
         pause = 1;
         clicks = 1;
-        a = b;
+        time_sg_click = time_db_click;
       }
     }
   };
-};
+});
