7267c622ecca1cbb183d5de4740edb4b4f6030f4
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-12-05 14:14:54 +0300

    task 2 upd

--- 07/02/main.js
+++ 07/02/main.js
@@ -1,43 +1,35 @@
-/* eslint-disable linebreak-style */
-
+const testButton = document.getElementById('test-button');
 const list = document.getElementById('list');
-let clicks = 0;
-let timeSgClick;
-let timeDbClick;
 let time;
-let pause = 0;
+let clickCount = 0;
 
-window.addEventListener('load', function doubleClick(element, timeDistance = 250) {
-  element = document.getElementById('test-button');
-  function getTimeStamp() {
+function singleClick() {
+  console.log('sgclick');
+}
+
+function getTimeStamp() {
     let now = new Date();
 
     return ((now.getMonth() + 1) + '-' + (now.getDate()) + '-' + now.getFullYear() + ' ' + now.getHours() + ':' + ((now.getMinutes() < 10) ? ('0' + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ('0' + now.getSeconds()) : (now.getSeconds())));
-  }
-  function doubleClickHandler(timeDate) {
-    let newLi = document.createElement('li');
+}
 
-    newLi.innerHTML = '2xClick - ' + timeDate;
-    list.appendChild(newLi);
-  }
+function doubleClick(timeDate) {
+  let newLi = document.createElement('li');
 
-  element.onclick = () => {
-    clicks++;
-    if (clicks === 1 && pause === 0) {
-      timeSgClick = new Date().getTime();
-      time = getTimeStamp();
+  newLi.innerHTML = '2xClick - ' + timeDate;
+  list.appendChild(newLi);
+}
+
+testButton.addEventListener('click', function() {
+    clickCount++;
+    if (clickCount === 1) {
+        singleClickTimer = setTimeout(function() {
+            clickCount = 0;
+        }, 1000);
+    } else if (clickCount === 2) {
+        clearTimeout(singleClickTimer);
+        time = getTimeStamp();
+        clickCount = 0;
+        doubleClick(time);
     }
-    if (clicks === 2) {
-      timeDbClick = new Date().getTime();
-      if (timeDbClick - timeSgClick < timeDistance) {
-        doubleClickHandler(time);
-        clicks = 0;
-        pause = 0;
-      } else {
-        pause = 1;
-        clicks = 1;
-        timeSgClick = timeDbClick;
-      }
-    }
-  };
-});
+}, false);
