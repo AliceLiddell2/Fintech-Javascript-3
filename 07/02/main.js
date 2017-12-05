12a0539153b9d4d06b2e4fe383113a91deddec60
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-12-05 14:47:07 +0300

    upd

--- 07/02/main.js
+++ 07/02/main.js
@@ -10,23 +10,27 @@ function getTimeStamp() {
   return new Date();
 }
 
-function doubleClick(timeDate) {
+function doubleClickHandler(timeDate) {
   let newLi = document.createElement('li');
 
   newLi.innerHTML = '2xClick - ' + timeDate;
   list.appendChild(newLi);
 }
 
-testButton.addEventListener('click', () => {
-  clickCount++;
-  if (clickCount === 1) {
-    singleClickTimer = setTimeout(() => {
+function doubleClick(element, timeDistance){
+  element.addEventListener('click', () => {
+    clickCount++;
+    if (clickCount === 1) {
+      singleClickTimer = setTimeout(() => {
+        clickCount = 0;
+      }, timeDistance);
+    } else if (clickCount === 2) {
+      clearTimeout(singleClickTimer);
+      time = getTimeStamp();
       clickCount = 0;
-    }, 1000);
-  } else if (clickCount === 2) {
-    clearTimeout(singleClickTimer);
-    time = getTimeStamp();
-    clickCount = 0;
-    doubleClick(time);
-  }
-}, false);
+      doubleClickHandler(time);
+    }
+  }, false);
+}
+
+doubleClick(testButton, 1000);
